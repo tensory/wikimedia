b4f90a90e13d38c7115532c841c910158937ed87
Ari Lacenski <alacenski@gmail.com>
2012-04-01 10:48:01 -0700

    Re-adding pretty fonts, adding images

--- index_files/script.js
+++ index_files/script.js
@@ -29,11 +29,11 @@
 			}
 		};
 	
-	/**
-	 * resetImageSizing
-	 * Forces images larger than the window to scale to 100% of available width
-	 * @param <object> images Image group to scale
-	 */
+		/**
+		 * resetImageSizing
+		 * Forces images larger than the window to scale to 100% of available width
+		 * @param <object> images Image group to scale
+		 */
        optimize.resetImageSizing = function(images) {
 	       $.each(images, function(index, img) {
 		       // On image load, resize it if needed
@@ -93,6 +93,43 @@
 			$('#showToggle').show();
 		};
 		
+		/**
+		 * Search function borrowed from http://www.nsftools.com/misc/SearchAndHighlight.htm 
+		 */ 		
+		var tagAll = function(bodyText, searchTerm) {  
+			// Find all occurrences of the search term in the given text,
+			// and add some "highlight" tags to them (we're not using a
+			// regular expression search, because we want to filter out
+			// matches that occur within HTML tags and script blocks, so
+			// we have to do a little extra validation)
+			var newText = "";
+			var i = -1;
+			var lcSearchTerm = searchTerm.toLowerCase();
+			var lcBodyText = bodyText.toLowerCase();
+		
+			while (bodyText.length > 0) {
+				i = lcBodyText.indexOf(lcSearchTerm, i+1);
+				if (i < 0) {
+					newText += bodyText;
+					bodyText = "";
+				} else {
+					// skip anything inside an HTML tag
+					if (bodyText.lastIndexOf(">", i) >= bodyText.lastIndexOf("<", i)) {
+					// skip anything inside a <script> block
+						if (lcBodyText.lastIndexOf("/script>", i) >= lcBodyText.lastIndexOf("<script", i)) {
+							newText += bodyText.substring(0, i) + finder.options.startTag + bodyText.substr(i, searchTerm.length) + finder.options.endTag;
+							bodyText = bodyText.substr(i + searchTerm.length);
+							lcBodyText = bodyText.toLowerCase();
+							i = -1;
+						}
+					}
+				}
+			}
+			return newText;
+		}
+
+		
+		// Getters and setters
 		function getSearchField() {
 			return $('#searchText');
 		};		
@@ -162,14 +199,7 @@
 		 */
 		finder.highlightAll = function(query) {
 		  	var queryArray = [query];
- /*
-  if (!document.body || typeof(document.body.innerHTML) == "undefined") {
-    if (warnOnFailure) {
-      alert("Sorry, for some reason the text of this page is unavailable. Searching will not work.");
-    }
-    return false;
-  }
-  */
+
 	  		var bodyText = $('#content')[0].innerHTML;
 	
 			for (var i = 0; i < queryArray.length; i++) {
@@ -179,41 +209,6 @@
 			$('#content')[0].innerHTML = bodyText;
 			return true;
 		};
-
-		/**
-		 * Search function borrowed from http://www.nsftools.com/misc/SearchAndHighlight.htm 
-		 */ 		
-		function tagAll(bodyText, searchTerm) {  
-			// Find all occurrences of the search term in the given text,
-			// and add some "highlight" tags to them (we're not using a
-			// regular expression search, because we want to filter out
-			// matches that occur within HTML tags and script blocks, so
-			// we have to do a little extra validation)
-			var newText = "";
-			var i = -1;
-			var lcSearchTerm = searchTerm.toLowerCase();
-			var lcBodyText = bodyText.toLowerCase();
-		
-			while (bodyText.length > 0) {
-				i = lcBodyText.indexOf(lcSearchTerm, i+1);
-				if (i < 0) {
-					newText += bodyText;
-					bodyText = "";
-				} else {
-					// skip anything inside an HTML tag
-					if (bodyText.lastIndexOf(">", i) >= bodyText.lastIndexOf("<", i)) {
-					// skip anything inside a <script> block
-						if (lcBodyText.lastIndexOf("/script>", i) >= lcBodyText.lastIndexOf("<script", i)) {
-							newText += bodyText.substring(0, i) + finder.options.startTag + bodyText.substr(i, searchTerm.length) + finder.options.endTag;
-							bodyText = bodyText.substr(i + searchTerm.length);
-							lcBodyText = bodyText.toLowerCase();
-							i = -1;
-						}
-					}
-				}
-			}
-			return newText;
-		}
 
 		/**
 		 * clearTags
